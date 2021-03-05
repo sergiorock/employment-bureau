bf536c114618cbadf1768bc01b63cf76739fc2cd
sergiorock <lopez.sergio.n@gmail.com>
2021-03-05 18:42:55 -0300

    add home controller and home view

--- config/routes.rb
+++ config/routes.rb
@@ -2,10 +2,12 @@ Rails.application.routes.draw do
   resources :companies
   resources :students
   devise_for :admins
+
+  resources :job_offers
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   #DEVISE NECESITA UN ROOT
-  root to: "job_offers#index"
+  root to: "home#index"
 
   resources :companies do
     resources :job_offers
