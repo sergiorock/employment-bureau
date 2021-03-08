6a4d867a31bf8f014ab13bfa80b8ec27d022e194
sergiorock <lopez.sergio.n@gmail.com>
2021-03-08 17:45:59 -0300

    add data set to company form

--- app/models/company.rb
+++ app/models/company.rb
@@ -4,6 +4,21 @@ class Company < ApplicationRecord
   validates :name, :cuit, :province, :city, :address, :phone, presence: true
   validates :email, format: { with: URI::MailTo::EMAIL_REGEXP }, presence: true
 
+  Provinces = {
+    "buenos_aires" => "Buenos Aires",
+    "cordoba" => "Córdoba",
+    "mendoza" => "Mendoza",
+    "entre_rios" => "Entre Ríos"
+  }
+
+  Cities = {
+    "caba" => "Capital Federal",
+    "rio_cuarto" => "Río Cuarto",
+    "florencio_varela" => "Florencio Varela",
+    "gualeguaychu" => "Gualeguaychú",
+    "guaymallen" => "Guaymallén"
+  }
+
   def self.search(search)
     if search.present?
       Company.where('name ILIKE :search OR province ILIKE :search OR city ILIKE :search OR email ILIKE :search', search: "%#{search.squish}%")
